9ab810f2411cdc631f2f95828c9fcafe93382d7e
Rachel Clark <rclark@kalittaair.com>
2019-04-19 14:52:51 -0400

    EOD commit to sync

--- router/routing.go
+++ router/routing.go
@@ -11,7 +11,7 @@ type Handle func(http.ResponseWriter, *http.Request, url.Values)
 //Router holds our routing trie.
 type Router struct {
 	tree     *node
-	redirect string
+	notFound  Handle
 }
 
 type node struct {
@@ -22,16 +22,16 @@ type node struct {
 }
 
 //NewRouter creates a new router and registers a redirect URL to be used in case we can't find a path. Generally a 404 page.
-func NewRouter(redirectURL string) *Router {
+func NewRouter(notFoundHandler Handle) *Router {
 	node := node{
-		component:    "root",
+		component:    "/",
 		isNamedParam: false,
 		methods:      make(map[string]Handle),
 	}
 
 	return &Router{
 		tree:     &node,
-		redirect: redirectURL,
+		notFound: notFoundHandler,
 	}
 }
 
@@ -39,4 +39,8 @@ func (n *node) addNode(method string, path string, handler Handle) {
 
 }
 
-func (n *node) traverse()
+func (n *node) traverse(components []string, params url.Values) (*node, string){
+	component := components[0]
+	if len(n.children <= 0 && n.component == component) return n, component
+	
+}
